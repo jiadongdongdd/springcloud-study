d5bbe982644c167ab9f5f7dd6fbcc8110b8e036b
ddjia <jiadongdongdd@126.com>
2019-12-30 10:16:08 +0800

    调整config-server、config-client作为微服务注册到服务中心，后续可通过负载均衡实现高可用的分布式配置中心

--- config-server/src/main/java/com/study/springcloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/study/springcloud/configserver/ConfigServerApplication.java
@@ -3,12 +3,15 @@ package com.study.springcloud.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * EnableConfigServer启动服务配置
+ * EnableEurekaClient 启动服务注册支持
  */
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
