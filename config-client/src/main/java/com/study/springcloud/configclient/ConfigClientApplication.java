361d8570cf475c26f8b554500025d498d31121e5
ddjia <jiadongdongdd@126.com>
2020-01-03 15:34:37 +0800

    Spring Cloud Bus 将分布式的节点用轻量的消息代理连接起来。它可以用于广播配置文件的更改或者服务之间的通讯，也可以用于监控。本文要讲述的是用Spring Cloud Bus实现通知微服务架构的配置文件的更改。

--- config-client/src/main/java/com/study/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/study/springcloud/configclient/ConfigClientApplication.java
@@ -2,9 +2,15 @@ package com.study.springcloud.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author lenovo
+ */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
--- config-client/src/main/java/com/study/springcloud/configclient/controller/ConfigController.java
+++ config-client/src/main/java/com/study/springcloud/configclient/controller/ConfigController.java
@@ -1,17 +1,23 @@
 package com.study.springcloud.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ * RefreshScope spring cloud提供的一种特殊的scope实现，用来实现配置、实例热加载
+ */
 @RestController
+@RefreshScope
 public class ConfigController {
 
-    @Value("${foo}")
-    String foo;
+    @Value("${server.name}")
+    String serverName;
 
     @RequestMapping("getConfig")
     public String getConfig(){
-        return foo;
+        return serverName;
     }
 }
